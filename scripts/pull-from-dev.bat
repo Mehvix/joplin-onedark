f244a63558d1b4e80705d5cf8872dd20e260b8f2
MehvixDesktop <mehvix@protonmail.com>
2021-05-25 11:11:24 -0500

    fixed list styling order

---
+++ scripts/pull-from-dev.bat
@@ -0,0 +1,5 @@
+print Going to pull FROM dev TO local... are you sure?
+pause
+copy C:\Users\maxdesktop\.config\joplindev-desktop\userchrome.css ..\userchrome.css
+copy C:\Users\maxdesktop\.config\joplindev-desktop\userstyle.css ..\userstyle.css
+pause
